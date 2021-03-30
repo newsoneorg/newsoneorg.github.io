aeff3a29d4e8471d4ae99717dce1ed90978d54a4
pranjalranjan <8675620+pranjalranjan@users.noreply.github.com>
2021-03-31 02:05:26 +0530

    Website update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+    $("#nav-bar-ul").append('<li><a href="/">HOME</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/politics.html">POLITICS</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/governance.html">GOVERNANCE</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/covid-19.html">COVID-19</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/corporate.html">CORPORATE</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/columns.html">COLUMNS</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/newsmaker.html">NEWSMAKER</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/youth.html">YOUTH</a></li>')
+    $("#nav-bar-ul").append('<li><a href="/potpourri.html">POTPOURRI</a></li>')
+    $("#nav-bar").css("background-color", "rgba(255, 102, 0, 0.726)")
+}
+)
