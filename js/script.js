b8cc1a51650a55fa46f6a6a7c2bc34ab0898694d
pranjalranjan <8675620+pranjalranjan@users.noreply.github.com>
2021-03-31 00:44:08 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('document').ready(function() {
+    $("#nav-bar-ul").append('<li><a href="NEWSONE.HTML">HOME</a></li>')
+    $("#nav-bar-ul").append('<li><a href="politics.html">POLITICS</a></li>')
+    $("#nav-bar-ul").append('<li><a href="governance.html">GOVERNANCE</a></li>')
+    $("#nav-bar-ul").append('<li><a href="covid-19.html">COVID-19</a></li>')
+    $("#nav-bar-ul").append('<li><a href="corporate.html">CORPORATE</a></li>')
+    $("#nav-bar-ul").append('<li><a href="columns.html">COLUMNS</a></li>')
+    $("#nav-bar-ul").append('<li><a href="newsmaker.html">NEWSMAKER</a></li>')
+    $("#nav-bar-ul").append('<li><a href="youth.html">YOUTH</a></li>')
+    $("#nav-bar-ul").append('<li><a href="potpourri.html">POTPOURRI</a></li>')
+}
+)
