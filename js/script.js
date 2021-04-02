83e466691dac4e753489a814bd4a7a84adfa3694
pranjalranjan <8675620+pranjalranjan@users.noreply.github.com>
2021-04-02 20:27:52 +0530

    Updates to website

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $('document').ready(function() {
-    $("#nav-bar-ul").append('<li><a href="/">HOME</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/politics.html">POLITICS</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/governance.html">GOVERNANCE</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/covid-19.html">COVID-19</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/corporate.html">CORPORATE</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/columns.html">COLUMNS</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/newsmaker.html">NEWSMAKER</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/youth.html">YOUTH</a></li>')
-    $("#nav-bar-ul").append('<li><a href="/potpourri.html">POTPOURRI</a></li>')
-    $("#nav-bar").css("background-color", "rgba(255, 102, 0, 0.726)")
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/">HOME</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/politics.html">POLITICS</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/governance.html">GOVERNANCE</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/covid-19.html">COVID-19</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/corporate.html">CORPORATE</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/columns.html">COLUMNS</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/newsmaker.html">NEWSMAKER</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/youth.html">YOUTH</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/potpourri.html">POTPOURRI</a></li>')
+
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toDateString();
 }
 )
