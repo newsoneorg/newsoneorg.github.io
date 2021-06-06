4930efd54cfad4ca2937d7defd6b0d3f9aa3878b
pranjalranjan <8675620+pranjalranjan@users.noreply.github.com>
2021-06-06 15:29:58 +0530

    Added hindi section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $('document').ready(function() {
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/">HOME</a></li>')
+    $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/hindi.html">हिंदी</a></li>')
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/politics.html">POLITICS</a></li>')
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/governance.html">GOVERNANCE</a></li>')
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/covid-19.html">COVID-19</a></li>')
@@ -8,6 +9,7 @@ $('document').ready(function() {
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/newsmaker.html">NEWSMAKER</a></li>')
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/youth.html">YOUTH</a></li>')
     $("#nav-bar-ul").append('<li class="nav-item active"><a class="nav-link" href="/potpourri.html">POTPOURRI</a></li>')
+
 
     var dt = new Date();
     document.getElementById("datetime").innerHTML = dt.toDateString();
